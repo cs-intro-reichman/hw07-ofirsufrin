e47154c9acfaa44432940119012975b3c9dcbc07
ofirsufrin <ofir.sufrin@post.runi.ac.il>
2025-12-24 20:43:29 +0200

    2

--- Koch.java
+++ Koch.java
@@ -27,7 +27,22 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
+		if (n == 0) {
+            StdDraw.line(x1, y1, x2, y2);
+            return;
+        }
+		double xa = x1 + (x2 - x1) / 3.0;
+        double ya = y1 + (y2 - y1) / 3.0;
+        double xb = x1 + 2.0 * (x2 - x1) / 3.0;
+        double yb = y1 + 2.0 * (y2 - y1) / 3.0;
+
+		double xc = (Math.sqrt(3) / 6.0) * (y1 - y2) + 0.5 * (x1 + x2);
+        double yc = (Math.sqrt(3) / 6.0) * (x2 - x1) + 0.5 * (y1 + y2);
+
+		curve(n - 1, x1, y1, xa, ya);
+		curve(n - 1, xa, ya, xc, yc);
+		curve(n - 1, xc, yc, xb, yb);
+		curve(n - 1, xb, yb, x2, y2);
 		//// Write the rest of your code below.
 	}
 
@@ -37,6 +52,12 @@ public class Koch {
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.1, y1 = 0.3;
+        double x2 = 1.0, y2 = 0.3;
+        double x3 = 0.55, y3 = 0.3 + Math.sqrt(3) / 2.0 * 0.9;
+
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
 	}
 }
